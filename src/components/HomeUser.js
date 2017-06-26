fb3d770c834acbcc845302397c5a90453ff038f3
Emily Ringoen <emilyaringoen@gmail.com>
2017-06-26 17:13:33 -0600

    fetch to alternatives made

--- src/components/HomeUser.js
+++ src/components/HomeUser.js
@@ -5,19 +5,20 @@ import { Cookies } from 'react-cookie'
 class HomeUser extends Component {
   constructor() {
     super()
-
+    this.state = {alt_ideas: ''}
   }
 
   componentWillMount() {
     let cookie = new Cookies()
     let user_id = cookie.get('id')
-    fetch(`http://localhost:3000/alternatives/${user_id}`, {
-      method: 'GET'
-    })
+
+    fetch(`http://localhost:3000/v1/alternatives/${user_id}`)
     .then(res => {
-      return res.text().then((alts) => {
-        alts = JSON.parse(alts)
-        console.log(alts)
+      return res.json().then((alts) => {
+        let alt_ideas = alts.map((alt) => {
+          return (<li key={alt.id}>{alt.text}</li>)
+        })
+        this.setState({alt_ideas: alt_ideas})
       })
     })
     }
@@ -28,6 +29,9 @@ class HomeUser extends Component {
         <SideNav />
         <header>
           <h2>Welcome</h2>
+          <ul>
+            {this.state.alt_ideas}
+          </ul>
         </header>
       </div>
     )
