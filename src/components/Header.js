22da0074991249b530b15313d11be6c9996fb853
Emily Ringoen <emilyaringoen@gmail.com>
2017-06-29 20:56:50 -0600

    fixed nabar a bit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react'
 import { Cookies } from 'react-cookie'
+import { Row, Col } from 'react-bootstrap'
 
 import '../assets/homeUser.css'
 import Sun from '../assets/images/sun.svg'
+import Man from '../assets/images/user.svg'
+
 
 class Header extends Component {
   constructor () {
@@ -36,8 +39,14 @@ class Header extends Component {
     return (
       <div>
         <div className="well well-large welcome text-center">
-          <h2>Emp <img className="text-center welcome-sun" src={ Sun } alt="empowerU"/> werU</h2>
-          <p>{this.state.name}</p>
+        <Row>
+          <Col sm={8}>
+            <h2>Emp <img className="text-center welcome-sun" src={ Sun } alt="empowerU"/> wer</h2>
+          </Col>
+          <Col sm={3}>
+            <p className="welcome_name"><img className="text-center welcome-user" src={ Man } alt="man logo"/> {this.state.name}</p>
+          </Col>
+        </Row>
         </div>
       </div>
     )
