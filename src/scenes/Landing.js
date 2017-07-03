ae080c33b4bf96f149cc311e2437ae1d813c4697
jenniferwiner <jeniwiner@gmail.com>
2017-07-03 11:44:48 -0600

    condense signup and signin components

--- src/scenes/Landing.js
+++ src/scenes/Landing.js
@@ -3,23 +3,32 @@ import { Link } from 'react-router-dom'
 
 import '../assets/Landing.css'
 import LoginForm from '../components/LoginForm'
+import SignupForm from '../components/SignupForm'
+
 
 class Landing extends Component {
   render() {
     return (
       <div className="background_image">
-        <div className="container-fluid">
           <div className="container-fluid splash">
             <div className="splash-content">
               <h2 className="text-center splash-heading">Empower</h2>
-              <h4 className="splash-tagline">clinician-led active platform for self management</h4>
-              <div className="login text-center">
-                <LoginForm/>
-                <h5><Link to="/signup">Create Account</Link></h5>
-              </div>
+              <h4 className="splash-tagline">an anxiety self-management platform</h4>
+
+              {window.location.pathname === '/' &&
+                <div className="login text-center">
+                  <LoginForm/>
+                  <h5><Link to="/signup">Create Account</Link></h5>
+                </div>
+              }
+              {window.location.pathname === '/signup' &&
+                <div className="login">
+                  <SignupForm/>
+                  <h5><Link to="/">Back to Login</Link></h5>
+                </div>
+              }
             </div>
           </div>
-        </div>
       </div>
     )
   }
